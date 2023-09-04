04f0d0dea73758c05a4404d7d99258a362378e87
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-09-04 16:34:39 +0300

    +

--- src/pages/ContactsPage.jsx
+++ src/pages/ContactsPage.jsx
@@ -3,7 +3,7 @@ import ContactList from '../components/ContactList/ContactList';
 import Modal from 'components/Modal/Modal';
 import Filter from '../components/Filter/Filter';
 import { useEffect } from 'react';
-import { getError, getIsLoading, getModal } from 'redux/contacts/selectors';
+import { getError, getModal } from 'redux/contacts/selectors';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/contact-operations';
 //import { Loader } from '../components/Loader/Loader';
