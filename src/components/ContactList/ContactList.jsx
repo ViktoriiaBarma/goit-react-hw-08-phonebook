2948ebcf1c1a0c8e84dca038d7a9ca694ee22b47
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-09-04 18:09:28 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Item, List } from './ContactList.styled';
 import { useSelector } from "react-redux";
-import { getContacts, getFilter } from "redux/contacts/selectors";
+import { getContacts } from "redux/contacts/selectors";
 import Contact from "../Contact/Contact";
 
 const ContactList = () => {
