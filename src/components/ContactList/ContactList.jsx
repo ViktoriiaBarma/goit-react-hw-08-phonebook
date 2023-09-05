459ffc05ec572ffaa47948dcc43fb2423e87553b
ViktoriiaBarma <barma.viktoria@gmail.com>
2023-09-05 12:47:45 +0300

    filter?

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,23 +1,31 @@
 import React from "react";
 import { Item, List } from './ContactList.styled';
 import { useSelector } from "react-redux";
-import { getContacts } from "redux/contacts/selectors";
+import { getContacts, getFilter } from "redux/contacts/selectors";
 import Contact from "../Contact/Contact";
 
 const ContactList = () => {
 
   const contacts = useSelector(getContacts);
    console.log(contacts);
-  // const filter = useSelector(getFilter);
-  //  console.log(filter);
-  // const filteredContacts = contacts.filter(contact =>
-  //   contact.name.toLowerCase().includes(filter.toLowerCase())
-  // );
+  const filtered = useSelector(getFilter);
+  console.log(filtered);
+  
+const normalizedFilter = filtered.toLowerCase();
+  const filteredContacts = contacts.filter(
+    ({ name, number }) =>
+      name.toLowerCase().trim().includes(normalizedFilter) ||
+      number.trim().includes(normalizedFilter)
+  );
 
+  //   const filteredContacts = contacts.filter(contact =>
+  //   contact.name.toLowerCase().includes(filtered.toLowerCase())
+  // );
+  
 
    return (
     <List>
-      {contacts?.map((contact) => {
+      {filteredContacts?.map((contact) => {
         return (
           <Item key={contact.id}>
              <Contact key={contact.id} contact={contact} />
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,49 +1,3 @@
-// import { useDispatch } from 'react-redux';
-// import { filterContact } from 'redux/contacts/filter-slice';
-// import { Input, LabelDescr } from './Filter.styled';
-// import { CloseBtn } from 'components/Modal/Modal.styled';
-// import { RxCross2 } from 'react-icons/rx';
-
-
-// const Filter = () => {
-//   const dispatch = useDispatch();
-
-//   const handleFilter = e => {
-//     if (e.currentTarget.value) {
-//       e.currentTarget.parentNode.children[2].classList.remove('hidden');
-//     } else {
-//       e.currentTarget.parentNode.children[2].classList.add('hidden');
-//     }
-//     dispatch(filterContact(e.currentTarget.value));
-//   };
-
-//  const clearFilter = e => {
-//     e.currentTarget.parentNode.children[1].value = '';
-//     dispatch(filterContact(e.currentTarget.parentNode.children[1].value));
-//     e.currentTarget.classList.add('hidden');
-//   };
-  
-//   return (
-//     <LabelDescr htmlFor="filter">
-//       Find contacts by name
-//       <Input  id="name"
-//               name="filter"
-//               type="text"
-//               placeholder="Search contact"
-//         onChange={handleFilter} />
-//        <CloseBtn onClick={clearFilter}
-//             >
-//               <RxCross2 />
-//             </CloseBtn>
-//     </LabelDescr>
-//   );
-// };
-
-// export default Filter;
-
-
-
-
 import { useDispatch } from 'react-redux';
 import { filterContact } from 'redux/contacts/filter-slice';
 import { Input, LabelDescr } from './Filter.styled';
@@ -51,7 +5,9 @@ import { Input, LabelDescr } from './Filter.styled';
 const Filter = () => {
    const dispatch = useDispatch()
   const hendleFilter = (e) => {
-    dispatch(filterContact(e.currentTarget.value))
+    const search = e.currentTarget
+    console.log(search.value)
+    dispatch(filterContact(search.value))
   }
 
   return (
